e7e989767eebd0828838489e1aee8a43d847df6d
Henk van Rooijen <henkiexxx4@gmail.com>
2016-11-05 18:43:35 +0100

    stylingg header

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app',
   template: `
-    <div>
-      <main-container></main-container>
+    <div class="main-container"> 
+    	<app-bar></app-bar>
+      	<main-container></main-container>
     </div>
   `
 })
